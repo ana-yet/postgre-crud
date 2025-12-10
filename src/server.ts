b642d365be8649f249be17752819d847ca458e79
Anayet <mdanayet.dev@gmail.com>
2025-12-10 11:37:03 +0600

    database connected && create user query, api for user post

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,57 @@
 import express from "express"
+import { Pool } from "pg";
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const app = express()
 const port = 5000;
 
 // body parsers
 app.use(express.json())
 
+const pool = new Pool({
+  connectionString: `${process.env.DB_URL}`
+})
+
+// initialize database
+const initDb = async ()=> {
+ await pool.query(` 
+   CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+    );
+    `)
+
+}
+
+initDb()
+
 app.get('/', (req : Request, res: Response) => {
   res.send('Hello Anayet')
+})
+
+app.get('/api/users',async (req : Request, res: Response) => {
+  const {name, email} = req.body;
+
+  try {
+    const result = await pool.query(`
+      INSERT INTO users (name, email) VALUES($1, $2) RETURNING *`, [name, email]);
+
+      res.status(401).json({
+        success : true,
+        message : "Data inserted successfully",
+        data : result.rows[0]
+      })
+  } catch (err: any) {
+    
+  }
 })
 
 
