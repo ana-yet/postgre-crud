5e5ddde8ecb8ca0792d5c76c80243c84c1878cd1
Anayet <mdanayet.dev@gmail.com>
2025-12-10 12:12:40 +0600

    updated user post method

--- src/server.ts
+++ src/server.ts
@@ -37,19 +37,23 @@ app.get('/', (req : Request, res: Response) => {
   res.send('Hello Anayet')
 })
 
-app.get('/api/users',async (req : Request, res: Response) => {
+app.post('/api/users',async (req : Request, res: Response) => {
   const {name, email} = req.body;
 
   try {
     const result = await pool.query(`
       INSERT INTO users (name, email) VALUES($1, $2) RETURNING *`, [name, email]);
 
-      res.status(401).json({
+      res.status(201).json({
         success : true,
         message : "Data inserted successfully",
         data : result.rows[0]
       })
   } catch (err: any) {
+    res.status(500).json({
+      success : false,
+      message : err.message,
+    })
     
   }
 })
